c09960c0a112c2f8c238efe3b9c72b87b4fdab54
Iván Vergara <ivan.francisco.vergara@gmail.com>
2023-02-19 21:01:38 -0300

    Corrección segunda pre-entrega
    Se agrego hoja de estilos y se corrigieron todas las indicaciones.

--- script.js
+++ script.js
@@ -6,28 +6,44 @@ class Nota {
     this.contenido = contenido;
   }
 
-  agregarNota() {
+  agregarNota() {  
     let titulo = document.getElementById("titulo").value;
     let contenido = document.getElementById("contenido").value;
-  
     if (titulo !== "" && contenido !== "") {
-      let nota = new Nota (titulo, contenido);
-      arreglo_notas.push(nota);
+      let nueva_nota = new Nota (titulo, contenido);
+      arreglo_notas.push(nueva_nota);
+      this.mostrarNota(nueva_nota);
+      document.getElementById("titulo").value = "";
+      document.getElementById("contenido").value = "";
     }
   }
 
-  mostrarNotas() {
-    let contenedor = document.getElementById("notas");
-    contenedor.innerHTML = "";
+  buscarNota() {
+    let indice_titulo = document.getElementById("buscador").value;
+    if (indice_titulo != ""){
+      let resultado_busqueda = arreglo_notas.find(nota => nota.titulo === indice_titulo);
+      this.mostrarNota(resultado_busqueda);
+    }
+    document.getElementById("buscador").value = "";
+  }
 
-    arreglo_notas.forEach(nota => {console.log(arreglo_notas)});
+  mostrarNota(nota) {
+    let contenedor = document.getElementById("notas");
+    if (nota){ 
+      contenedor.innerHTML = `<h2>${nota.titulo}</h2><p>${nota.contenido}</p>`;
+    } else {
+      contenedor.innerHTML = `<h2>Lo siento, pero "${document.getElementById("buscador").value}" no aparece en la lista</h2>`;
+    }
   }
 
 }
 
-let nota_nueva = new Nota (prompt("Ingrese un título"), prompt("Ingrese el contenido"));
-arreglo_notas.push(nota_nueva);
-        
-nota_nueva.mostrarNotas();
+// Eventos
+function init() {
+  let nueva_nota = new Nota();
+  document.getElementById("agregar_nota").addEventListener("click", function() {nueva_nota.agregarNota();});
+  document.getElementById("buscar_nota").addEventListener("click", function() {nueva_nota.buscarNota();});
+}
 
+init();
 
